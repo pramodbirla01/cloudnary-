2b84ac2f8fb82906b84a8602fe63e86c1c126b2a
pramodbirla01 <prmdbirla65@gmail.com>
2025-08-27 17:58:51 +0530

    cors

--- upload/src/app.ts
+++ upload/src/app.ts
@@ -1,11 +1,19 @@
 import express from "express";
+import cors from "cors";
 import dotenv from "dotenv";
 import uploadRoutes from "./routes/uploadRoutes";
 
 dotenv.config();
 
+
 const app = express();
 app.use(express.json());
+
+// Allow CORS for frontend on port 3000
+app.use(cors({
+	origin: "http://localhost:3000",
+	credentials: true
+}));
 
 
 app.use("/api", uploadRoutes);
